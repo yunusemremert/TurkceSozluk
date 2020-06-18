9d0c8fad5b8cde40e8dd40a868d9f3a77344d9d7
Yunus Emre Mert <yunusemremert@yandex.com>
2020-06-18 17:16:58 +0300

    Edit problem useEffect

--- src/views/detail.js
+++ src/views/detail.js
@@ -23,7 +23,7 @@ function DetailView({ route }) {
     }, [])
   )
 
-  const getDetailData = async () => {
+  const getDetailData = async keyword => {
     const response = await fetch(`https://sozluk.gov.tr/gts?ara=${keyword}`)
     const data = await response.json()
 
@@ -31,8 +31,8 @@ function DetailView({ route }) {
   }
 
   React.useEffect(() => {
-    getDetailData()
-  }, [])
+    getDetailData(keyword)
+  }, [keyword])
 
   return (
     <Box as={SafeAreaView} bg="softRed" flex={1}>
